bc6f3e9fa5ad5c6894fb17876a000f2b5952a873
Victor Hugo de Lira Silva <victorhdelira123@gmail.com>
2024-11-23 01:24:47 +0000

    teste de commit

---
+++ Lista_de ATV01/questao_04.py
@@ -0,0 +1,15 @@
+dia_inicial = int(input('Informe o dia incial: '))
+mes_inicial = int(input('Informe o mês inicial: '))
+
+dia_final = int(input('Informe o dia final: '))
+mes_final = int(input('Informe o mês final: '))
+
+
+
+if mes_inicial == mes_final:
+    diferenca_dias = dia_final - dia_inicial
+else:
+    diferenca_dias = ((mes_final - mes_inicial) * 30) - (dia_final - dia_inicial)
+
+#Aqui considerando que todos os meses têm 30 dias.
+print (f'Passaram-se {diferenca_dias} dia(s).')
