1d46995493a1853db33a7266b5f60caeedd2b5f7
Victor Hugo de Lira Silva <victorhdelira123@gmail.com>
2024-11-24 19:55:58 +0000

    commit

--- Lista_de ATV01/questao_05.py
+++ Lista_de ATV01/questao_05.py
@@ -5,34 +5,33 @@
 #Faça um programa que leia o número de minutos que um veículo permaneceu no estacionamento e exiba
 #o valor a ser pago pelo responsável.
 #Como exemplo, considere que o carro ficou 310 minutos no estacionamento; deve pagar: R$ 16,00 (pelas
-#duas primeiras horas) + R$ 10,00 (pela terceira e quarta horas) + R$ 6,00 (pela quinta hora e fração da
-#sexta hora): total de R$ 32,00
+#duas primeiras horas) + R$ 10,00 (pela terceira e quarta horas) + R$ 6,00 (pela quinta hora e fração da sexta hora): total de R$ 32,00
 
 
 
 
-tempo = int(input('Digite o tempo em minutos que voce ficou no estacionamento: '))
+minuto = int(input('Digite o tempo em minutos que voce ficou no estacionamento: '))
 
+tempo = minuto / 60
 valor_pagar = 0
 
 
+if tempo <= 2 :     
+    valor_pagar = tempo * 8
+   
+elif tempo > 2 and tempo < 4 :
+    valor_pagar = 16 + ((tempo - 2) * 5)
 
+elif tempo > 4 and tempo < 12 :
+    valor_pagar = 26 + ((tempo - 4) * 6)
 
-if tempo <= 60 :     
-    valor_pagar += 8
-    print(f'O valor a ser pago é de R${valor_pagar}')
+else:
+    valor_pagar = 30 + ((tempo - 12) * 6)
 
-elif tempo <= 120 :
-    valor_pagar += (8*2)
-    print(f'O valor a ser pago é de R${valor_pagar}')
+    
 
-elif tempo <= 180 :
-    valor_pagar += 5
-    print(f'O valor a ser pago é de R${valor_pagar}')
+print(f'O valor a ser pago é de R${valor_pagar:.2f}')
 
-elif tempo <= 240 :
-    valor_pagar += (5*2)
-    print(f'O valor a ser pago é de R${valor_pagar}')
     
     
 
