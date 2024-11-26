90a8a259ce2075506908e385fc91cdfe62300670
Victor Hugo de Lira Silva <victorhdelira123@gmail.com>
2024-11-26 20:33:03 -0300

    Add files via upload

--- aula 26.11 - laços de repetição/exemplo 01.py
+++ aula 26.11 - laços de repetição/exemplo 01.py
@@ -1,1 +1,14 @@
+# exemplos laços de repetição com while
 
+valor = int(input('Digite um valor:'))
+# exemplo de tabela de multiplicação
+multiplicador = 1
+
+print(f' Tabuada do numero {valor}: ')
+while multiplicador <= 10: 
+    # enquanto o valor for verdadeiro ele permanece no laço, e quando for falso ele segue com o que está abaixo do codigo 
+
+    print(f' {multiplicador} x {valor} = { multiplicador * valor}')
+    multiplicador += 1
+
+print('fim da tabuada!')
---
+++ aula 26.11 - laços de repetição/exemplo 02.py
@@ -0,0 +1,8 @@
+# calculando e exibindo a soma dos 100 primeiros numeros inteiros positivos 
+
+soma = 0 
+x = 1 
+while x <= 100:
+    soma += x 
+    x += 1 
+print(f' A soma dos 100 primeiros numeros positivos é {soma}')
