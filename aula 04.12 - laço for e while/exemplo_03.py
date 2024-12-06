a9b4028f27f937e3e0f081c5bfc57140afb96b3e
Victor Hugo de Lira Silva <victorhdelira123@gmail.com>
2024-12-06 23:13:01 +0000

    commit em aula!

--- aula 04.12 - laço for e while/exemplo_03.py
+++ aula 04.12 - laço for e while/exemplo_03.py
@@ -8,3 +8,13 @@ num = int(input('DIgite um numero: '))
 
 if num <= 0:
     sys.exit('Digite um valor positivo!')
+
+strbinario = ''
+
+while num > 0 :
+    resto = num % 2
+    strbinario = str(resto) + strbinario 
+    num = num // 2
+    
+print(strbinario)
+    
