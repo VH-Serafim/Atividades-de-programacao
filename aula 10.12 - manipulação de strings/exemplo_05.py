3b540bd430fa3daefa1ac5b9c6c0dd75e153086a
Victor Hugo de Lira Silva <victorhdelira123@gmail.com>
2024-12-12 00:38:08 +0000

    Commit em aula!

--- aula 10.12 - manipulação de strings/exemplo_05.py
+++ aula 10.12 - manipulação de strings/exemplo_05.py
@@ -3,3 +3,12 @@
    Fazer um programa em que o usuário informe uma string e depois uma palavra
    e em seguida o programa informe se a palavra informada está na string informada
 '''
+
+frase = input('Digite a uma frase: ')
+
+palavra = input('Digite uma palavra: ')
+
+if palavra.lower() in frase.lower():
+   print(f'a palavra {palavra} está na frase! ')
+else: 
+   print(f'a palavra {palavra} não está na frase!')
