7d0500749b57c28e680e3763c3f2cfc02e056dfe
Victor Hugo de Lira Silva <victorhdelira123@gmail.com>
2025-02-16 02:30:38 +0000

    commit

---
+++ aula 12.02.25 - dicionarios /exemplo_07.py
@@ -0,0 +1,19 @@
+'''
+   EXEMPLO 07:
+
+   Usando o dicionário dictAlunos faça um programa que solicite ao usuário a matrícula de um 
+   aluno:
+   
+    - Caso a matrícula não exista no dicionário, o programa deverá solicitar as duas notas 
+    da disciplina e adicione os valores digitados no dicionário;
+
+    - Caso a matrícula já exista no dicionário, o programa deverá exibir a mensagem "Matrícula já  existe!" 
+    mostrar as notas cadastradas e solicitar as novas notas.  
+
+   O programa deve continuar solicitando a matrícula e as notas até que o usuário digite a
+   matrícula 0.
+
+   Ao final, o programa deve exibir o dicionário dictAlunos.
+'''
+
+dictAlunos = dict()
